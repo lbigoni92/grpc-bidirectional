2ce6d166d9d14c1316d2244141ae8262ee5e24cf
Luca <luca.bigoni@apparound.com>
2025-06-30 08:32:23 +0200

    fix

--- server/main.go
+++ server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 
 	pb "grpc-bidirectional/proto"
@@ -12,29 +14,39 @@ import (
 
 type server struct {
 	pb.UnimplementedCommunicatorServer
+	mu      sync.Mutex
+	streams map[pb.Communicator_ChatServer]struct{}
 }
 
-func (s *server) Chat(stream pb.Communicator_ChatServer) error {
-	log.Println("Server: Chat stream opened")
-	go func() {
-		for {
-			time.Sleep(100 * time.Millisecond)
-			msg := &pb.Message{
-				Sender:    "server",
-				Content:   "Evento lato server",
-				Timestamp: time.Now().Unix(),
-			}
-			if err := stream.Send(msg); err != nil {
-				log.Println("Errore invio server:", err)
-				return
-			}
+func (s *server) broadcast(msg *pb.Message) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for st := range s.streams {
+		if err := st.Send(msg); err != nil {
+			log.Println("Errore broadcast, rimuovo stream:", err)
+			delete(s.streams, st)
 		}
+	}
+}
+func (s *server) Chat(stream pb.Communicator_ChatServer) error {
+	// Registra lo stream
+	s.mu.Lock()
+	s.streams[stream] = struct{}{}
+	s.mu.Unlock()
+	log.Println("Client connesso, stream registrato")
+
+	// Assicura deregistrazione alla chiusura dello stream
+	defer func() {
+		s.mu.Lock()
+		delete(s.streams, stream)
+		s.mu.Unlock()
+		log.Println("Client disconnesso, stream rimosso")
 	}()
 
+	// Ciclo di ricezione messaggi client
 	for {
 		in, err := stream.Recv()
 		if err != nil {
-			log.Println("Client disconnesso:", err)
 			return err
 		}
 		log.Printf("Messaggio ricevuto: %s: %s\n", in.Sender, in.Content)
@@ -48,7 +60,21 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterCommunicatorServer(grpcServer, &server{})
+	s := &server{streams: make(map[pb.Communicator_ChatServer]struct{})}
+	pb.RegisterCommunicatorServer(grpcServer, s)
+
+	go func() { // Esecuzione simulata della pipeline
+		for i := 0; i < 10; i++ {
+			log.Printf("Esecuzione pipeline step %d", i)
+			time.Sleep(3 * time.Second)
+			evt := &pb.Message{
+				Sender:    "server",
+				Content:   fmt.Sprintf("Pipeline step %d completato", i),
+				Timestamp: time.Now().Unix(),
+			}
+			s.broadcast(evt)
+		}
+	}()
 	log.Println("Server in ascolto su :50051")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Errore avvio server: %v", err)
